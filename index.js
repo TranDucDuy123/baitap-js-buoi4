0726cc864c395588a2f95be8e0c614a004e4a470
Duy Tran Duc <dyly399@gmail.com>
2022-12-01 20:36:36 +0700

    sua bai 1

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*ex-1
+input:
+  + nhận giá trị 3 số nguyên từ người nhập
+algorithm:
+  + tạo 3 biến so1EL,so3EL,so3EL nhận 3 giá trị số nguyên
+  + tạo 3 viến so1,so2,so3 giả sử 3 số có giá trị tăng dần
+  + so sánh so1EL,so2EL giá trị nào nhỏ thì gán cho so1, lớn hơn thì gán cho so2
+  + so so sánh so3EL:
+    - nếu nhỏ hơn hoặc bằng thì 
+      temp = so1; so1 =so3EL; so3=so2; so2=temp
+    - nếu lớn hơn
+output:
+*/
 function xuat() {
   var so1EL = document.getElementById("txt-so1").value * 1;
   var so2EL = document.getElementById("txt-so2").value * 1;
@@ -6,16 +19,23 @@ function xuat() {
   var so1, so2, so3;
   var temp;
   so1 = so1EL;
+
   if (so2EL < so1) {
     so1 = so2EL;
     so2 = so1EL;
   } else so2 = so2EL;
-  if (so3EL < so1) {
-    temp = so1;
-    so1 = so3EL;
+
+  if (so3EL >= so2) {
+    so3 = so3EL;
+  } else if (so3EL <= so1) {
     so3 = so2;
-    so2 = temp;
-  } else so3 = so3EL;
+    so2 = so1;
+    so1 = so3EL;
+  } else {
+    so3 = so2;
+    so2 = so3EL;
+  }
+
   outputEL = document.getElementById("output1");
   var ketqua = so1 + " - " + so2 + " - " + so3;
   outputEL.innerHTML = ketqua;
